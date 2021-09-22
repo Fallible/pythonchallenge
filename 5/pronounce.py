eba73de486ec1342cc0690dc04e930847f133827
fallible <snewbill@gmail.com>
2021-09-21 22:00:10 -0600

    solution and writeup for 5

--- 5/pronounce.py
+++ 5/pronounce.py
@@ -1,2 +1,7 @@
-import urllib
+from urllib.request import urlopen
 import pickle
+
+raw = urlopen('http://www.pythonchallenge.com/pc/def/banner.p')
+data = pickle.load(raw)
+for stuff in data:
+    print("".join([k * v for k, v in stuff]))
